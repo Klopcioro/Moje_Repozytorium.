7656e7244fe87ae7b5b1c45e75f9b81ad533af78
Klopcioro <65850980+Klopcioro@users.noreply.github.com>
2021-04-22 17:22:21 +0200

    Add files via upload
    Added first debuff code (floating).

---
+++ TMDebuff.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TM.Buffs 
+{
+    class TMDebuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("TM debuff");
+            
+        }
+
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            
+            for (int i = 0; i <=100; i++ )
+            {
+                //float velXMult = 1f;
+
+                //npc.velocity.X *= velXMult;
+
+                float velYmult = 5f;
+
+                npc.velocity.Y -= velYmult;
+            }
+            
+            
+        }
+    }
+}
